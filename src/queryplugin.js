ef7c03099d967eaaa4e65710b94011762b1a21ad
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-12-13 22:37:38 +0800

    remove xtra file

--- src/queryplugin.js
+++
@@ -1,9 +0,0 @@
-import { QueryClient } from 'react-query/core'
-
-const QueryClientSymbol = Symbol()
-
-export function provideQueryClient() {
-  const queryClient = new QueryClient();
-
-  provide(QueryClientSymbol, queryClient);
-}
